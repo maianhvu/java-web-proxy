37e8d377d07424dca0c4b4a847dd51992dfa2530
Mai Anh Vu <me@maianhvu.com>
2015-09-23 13:33:10 +0800

    add extra documentation for response

--- Response.java
+++ Response.java
@@ -4,14 +4,23 @@ public class Response {
 
   private InputStream dataSource;
 
+  // Private constructor
   private Response(InputStream inputStream) {
     this.dataSource = inputStream;
   }
 
+  /**
+   * Static Constructor.
+   * Alias for private constructor, just for better readability
+   */
   public static Response read(InputStream stream) {
     return new Response(stream);
   }
 
+  /**
+   * Start forwarding request from the data source initialized to
+   * Accept multiple output streams
+   */
   public void forward(OutputStream... dests) throws IOException {
     byte[] b = new byte[8192];
     int len;
